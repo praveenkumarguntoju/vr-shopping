9aef87fae0ad7128483fa68da93cce509591500a
Praveen Kumar Guntoju <gpravin999@gmail.com>
2018-05-23 16:05:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var path = require('path');
 
   
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(__dirname + 'images'));
 app.use(express.static(__dirname + 'jsfiles'));
 app.use(express.static(__dirname + '/styles'));
