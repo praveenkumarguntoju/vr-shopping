7c14576cb19c9e3de25194947465dcd5f67aa225
Praveen Kumar Guntoju <gpravin999@gmail.com>
2018-05-23 15:45:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static('public'))
 app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/jsfiles'));
 app.use(express.static(__dirname + '/styles'));
