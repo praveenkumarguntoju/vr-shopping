5e952164649a96124d6c859ff6a111c6bfe4e8e8
Praveen Kumar Guntoju <gpravin999@gmail.com>
2018-08-13 16:11:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.static(__dirname + '/aframefiles'));
 // app.use(express.static(__dirname + "/toshiba_satellite_laptop"));
 
 // app.use(express.static(__dirname + "/viking_helmet"));
-app.use(express.static(__dirname + "/chair"));
+// app.use(express.static(__dirname + "/chair"));
+app.use(express.static(__dirname + "/carpet"));
 app.use(express.static(__dirname + '/jsfiles'));
 app.use(express.static(__dirname + '/styles'));
 app.use(express.static(__dirname + '/phpfiles'));
